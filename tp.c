d310cc9f81e9dd54571c9772ba22ff0cfda04420
Yanne <nanefc@hotmail.com>
2021-04-16 01:55:31 -0300

    commit

--- tp.c
+++ tp.c
@@ -147,35 +147,9 @@ void cadastro_aluguel(ALUGUEL* lista_aluguel) {
 	printf("Digite a data do aluguel (dd/mm): ");
 	setbuf(stdin, NULL);
 	scanf("%d/%d", &lista_aluguel->data.dia, &lista_aluguel->data.mes);
-}
-
-void exclusao_aluguel(ALUGUEL* lista_aluguel) {
-	ALUGUEL busca_aluguel;
-	ALUGUEL *atual = lista_aluguel, *antecessor = NULL;
-	printf("Digite o nome do livro: ");
-	setbuf(stdin, NULL);
-	fgets(busca_aluguel.livro.nome, 50, stdin);
-	printf("Digite o nome do aluno: ");
-	setbuf(stdin, NULL);
-	fgets(busca_aluguel.aluno.nome, 50, stdin);
-	while (lista_aluguel->prox_ALUGUEL != NULL) {
-		printf("%s", lista_aluguel->aluno.nome);
-		printf("%s", lista_aluguel->livro.nome);
-		if (strcmp(busca_aluguel.aluno.nome, lista_aluguel->aluno.nome) == 0) {
-			if (strcmp(busca_aluguel.livro.nome, lista_aluguel->livro.nome) == 0) {
-				if (antecessor == NULL) {
-					lista_aluguel = atual->prox_ALUGUEL;
-				} else {
-					antecessor = atual->prox_ALUGUEL;
-				}
-				printf("Aluguel excluído com sucesso!\n");
-			}
-		} else {
-			antecessor = atual;
-			atual = atual->prox_ALUGUEL;
-		}
-	}
-	printf("Nenhum aluguel foi encontrado!\n");
+	lista_aluguel->prox_ALUGUEL = malloc(sizeof(ALUGUEL));
+	lista_aluguel = lista_aluguel->prox_ALUGUEL;
+	lista_aluguel->prox_ALUGUEL = NULL;
 }
 
 void busca_aluguel(ALUGUEL* lista_aluguel) {
@@ -207,11 +181,47 @@ void busca_aluguel(ALUGUEL* lista_aluguel) {
 	printf("Nenhum resultado foi encontrado!\n");
 }
 
+void exclusao_aluguel(ALUGUEL* lista_aluguel) {
+	ALUGUEL busca_aluguel;
+	ALUGUEL *atual = lista_aluguel, *antecessor;
+	printf("Digite o nome do livro: ");
+	setbuf(stdin, NULL);
+	fgets(busca_aluguel.livro.nome, 50, stdin);
+	printf("Digite o nome do aluno: ");
+	setbuf(stdin, NULL);
+	fgets(busca_aluguel.aluno.nome, 50, stdin);
+	while (lista_aluguel != NULL) {
+		printf("Yanne1");
+		if (strcmp(busca_aluguel.aluno.nome, lista_aluguel->aluno.nome) == 0) {
+			printf("Yanne2");
+			if (strcmp(busca_aluguel.livro.nome, lista_aluguel->livro.nome) == 0) {
+				antecessor->prox_ALUGUEL = atual->prox_ALUGUEL;
+				return;
+			}
+		} 
+		printf("Yanne3");
+		antecessor = atual;
+		atual = atual->prox_ALUGUEL;
+	}
+	printf("Nenhum aluguel foi encontrado!\n");
+}
+
 void quantidade_aluguel(ALUGUEL* lista_aluguel) {
+	ALUGUEL *aux = lista_aluguel;
+	int i = 0;
+	while (aux->prox_ALUGUEL != NULL) {
+		i++;
+		aux = aux->prox_ALUGUEL;
+	}
+	printf("Quantidade: %d\n", i);
 }
 
 void impressao_aluguel(ALUGUEL* lista_aluguel) {
-	//while (lista_aluguel->prox_ALUGUEL != NULL)
+	ALUGUEL *aux = lista_aluguel;
+	while (aux->prox_ALUGUEL != NULL) {
+		printf("Nome do livro:%sNome do aluno: %sData: %d/%d\n\n", aux->livro.nome, aux->aluno.nome, aux->data.dia, aux->data.mes);
+		aux = aux->prox_ALUGUEL;
+	}
 }
 
 /////Funções da entidade aluno/////
@@ -232,21 +242,67 @@ void cadastro_aluno(ALUNO* lista_aluno) {
 	setbuf(stdin, NULL);
 	scanf("%d", &aux_lista->idade);
 	setbuf(stdin, NULL);
-	aux_lista->prox_ALUNO = malloc(sizeof(ALUNO));
-	aux_lista = aux_lista->prox_ALUNO;
-	aux_lista->prox_ALUNO = NULL;
+	lista_aluno->prox_ALUNO = malloc(sizeof(ALUNO));
+	lista_aluno = lista_aluno->prox_ALUNO;
+	lista_aluno->prox_ALUNO = NULL;
 }
 
 void exclusao_aluno(ALUNO* lista_aluno) {
 }
 
 void busca_aluno(ALUNO* lista_aluno) {
+	char nome1[50], nome2[50];
+	ALUNO *aux = lista_aluno, busca_aluno;
+	printf("Digite o nome do aluno: ");
+	setbuf(stdin, NULL);
+	fgets(busca_aluno.nome, 50, stdin);
+	printf("Digite a idade do aluno: ");
+	setbuf(stdin, NULL);
+	scanf("%d", &busca_aluno.idade);
+	while (aux != NULL) {
+		strcpy(nome1, aux->nome);
+		//strcpy(nome2, aux->idade);
+		printf("Yanne1");
+		if (strcmp(nome1, busca_aluno.nome) == 0) {
+			printf("Yanne2");
+			if (lista_aluno->idade == busca_aluno.idade) {
+				printf("Yanne3");
+				printf("O aluno %s foi encontrado!\n", busca_aluno.nome);
+				break;
+			}
+		}
+		else {
+			printf("Yanne4");
+			aux = aux->prox_ALUNO;
+		}
+	}
+	printf("Nenhum resultado foi encontrado!\n");
 }
 
 void quantidade_aluno(ALUNO* lista_aluno) {
+	ALUNO *aux = lista_aluno;
+	int i = 0;
+	while (aux->prox_ALUNO != NULL) {
+		i++;
+		aux = aux->prox_ALUNO;
+	}
+	printf("Quantidade: %d\n", i);
 }
 
 void impressao_aluno(ALUNO* lista_aluno) {
+	ALUNO *aux = lista_aluno;
+	while (aux->prox_ALUNO != NULL) {
+		if (aux->turno == 'm' || aux->turno == 'M') {
+			printf("Nome:%sIdade: %d\nTurno: Matutino\n", aux->nome, aux->idade);
+		}
+		else if (aux->turno == 'V' || aux->turno == 'v') {
+			printf("Nome:%sIdade: %d\nTurno: Vespertino\n", aux->nome, aux->idade);
+		}
+		else if (aux->turno == 'I' || aux->turno == 'i') {
+			printf("Nome:%sIdade: %d\nTurno: Integral\n", aux->nome, aux->idade);
+		}
+		aux = aux->prox_ALUNO;
+	}
 }
 
 /////Funções da entidade livro/////
@@ -263,16 +319,62 @@ void cadastro_livro(LIVRO* lista_livro) {
 	printf("Digite o numero da edicao: ");
 	setbuf(stdin, NULL);
 	scanf("%d", &lista_livro->edicao);
+	lista_livro->prox_LIVRO = malloc(sizeof(LIVRO));
+	lista_livro = lista_livro->prox_LIVRO;
+	lista_livro->prox_LIVRO = NULL;
 }
 
 void exclusao_livro(LIVRO* lista_livro) {
 }
 
 void busca_livro(LIVRO* lista_livro) {
+	char nome1[50], nome2[50];
+	LIVRO *aux = lista_livro, busca_livro;
+	printf("Digite o nome do livro: ");
+	setbuf(stdin, NULL);
+	fgets(busca_livro.nome, 50, stdin);
+	printf("Digite o nome do autor: ");
+	setbuf(stdin, NULL);
+	fgets(busca_livro.autor, 50, stdin);
+	printf("Digite o numero da edicao: ");
+	scanf("%d", &busca_livro.edicao);
+	while (aux != NULL) {
+		strcpy(nome1, aux->nome);
+		strcpy(nome2, aux->autor);
+		printf("Yanne1");
+		if (strcmp(nome1, busca_livro.nome) == 0) {
+			printf("Yanne2");
+			if (strcmp(nome2, busca_livro.autor) == 0) {
+				printf("Yanne3");
+				if (aux->edicao == busca_livro.edicao) {
+					printf("Yanne4");
+					printf("O livro %s foi encontrado!\n", busca_livro.nome);
+					break;
+				}
+			}
+		}
+		else {
+			printf("Yanne5");
+			aux = aux->prox_LIVRO;
+		}
+	}
+	printf("Nenhum resultado foi encontrado!\n");
 }
 
 void quantidade_livro(LIVRO* lista_livro) {
+	LIVRO *aux = lista_livro;
+	int i = 0;
+	while (aux->prox_LIVRO != NULL) {
+		i++;
+		aux = aux->prox_LIVRO;
+	}
+	printf("Quantidade: %d\n", i);
 }
 
 void impressao_livro(LIVRO* lista_livro) {
+	LIVRO *aux = lista_livro;
+	while (aux->prox_LIVRO != NULL) {
+		printf("Nome:%sAutor: %sEdicao: %d\n\n", aux->nome, aux->autor, aux->edicao);
+		aux = aux->prox_LIVRO;
+	}
 }
